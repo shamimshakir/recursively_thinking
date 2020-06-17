f80ed2420bf856f28a592fbf62b710cfce5563b8
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-17 18:25:47 +0600

    Update think_recursively.js

--- think_recursively.js
+++ think_recursively.js
@@ -60,3 +60,15 @@ var isEven = function(n) {
     }
 };
 isEven(8) // true
+
+
+
+
+// 5. Sum all integers below a given integer.
+// sumBelow(10); // 45
+// sumBelow(7); // 21
+var sumBelow = function(n) {
+    if(n <= 0)return 0;
+    return n + sumBelow(n - 1) - 1
+};
+sumBelow(10)  //45
